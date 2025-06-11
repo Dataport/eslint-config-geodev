879fdc433ac8bf842c0a40665aae150f2d6b068d
Hendrik Oenings <142312676+oeninghe-dataport@users.noreply.github.com>
2025-06-11 12:38:37 +0200

    Update 2025-06-11

--- configs/index.js
+++ configs/index.js
@@ -6,25 +6,16 @@ export default [
 	...neostandard(),
 	{
 		rules: {
-			'comma-dangle': ['error', 'always-multiline'],
 			'no-console': ['error', { allow: ['warn', 'error'] }],
-			'no-debugger': 'error',
 			curly: 'error',
 			'no-else-return': 'error',
-			'no-lonely-if': 'error',
-			'require-await': 'error',
 			'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
-			'no-eval': 'error',
-			'no-implied-eval': 'error',
 			complexity: 'warn',
 			'max-depth': 'warn',
 
+			'@stylistic/comma-dangle': ['error', 'always-multiline'],
 			'@stylistic/indent': ['error', 'tab'],
 			'@stylistic/no-tabs': 'off',
-			'@stylistic/no-multi-spaces': 'error',
-			'@stylistic/brace-style': 'error',
-			'@stylistic/no-extra-parens': 'error',
-			'@stylistic/eol-last': 'error',
 
 			'import-x/order': 'error',
 		},
